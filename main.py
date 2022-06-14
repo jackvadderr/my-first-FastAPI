a59995b658e4f0355e85cfee6f46b8ea5f34b03e
jackvadderr <jacjacckck@gmail.com>
2022-06-14 17:06:52 -0400

    Primeira API com FastAPI

---
+++ main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+
+@app.get('/')
+async def msg():
+    return{"msg": "Minha primeira API com FastAPI"}
+
+
+if __name__ == '__main__':
+    import uvicorn
+
+    uvicorn.run("main:app", host="0.0.0.0", port=8000,
+                log_level="info", reload=True)
